71324c32214fd85b7d639785edc5b91337d0cd0e
Carlos Rueda <carueda@mbari.org>
2017-08-25 13:35:05 -0700

    simplify details panel

--- js/src/main/scala/gpdviz/webapp/View.scala
+++ js/src/main/scala/gpdviz/webapp/View.scala
@@ -4,7 +4,6 @@ import com.thoughtworks.binding.Binding.Constants
 import com.thoughtworks.binding.{Binding, dom}
 import gpdviz.model.VmDataStream
 import org.scalajs.dom.raw.Node
-import pprint.PPrinter.BlackWhite.{apply ⇒ pp}
 
 case class ChartDiv(
                    id:          String,
@@ -26,7 +25,8 @@ class View(vm: VModel) {
 
   @dom private val sysDesc = <span>{ vm.ss.bind.description.getOrElse("") }</span>
 
-  @dom private val absoluteCharts = <div>
+  @dom private val absoluteCharts =
+    <div>
     {
       vm.absoluteCharts map { c ⇒
         //console.log("ADDING absoluteChart div id=" + c.id)
@@ -37,56 +37,57 @@ class View(vm: VModel) {
         </div>
       }
     }
-  </div>
+    </div>
 
-  @dom private def sysActivity(): Binding[Node] = {
+  @dom private def sysActivity(): Binding[Node] =
     <div>
-      <div>Center: { vm.ss.bind.center.map(_.toString).getOrElse("") }</div>
+      <div>{ vm.ss.bind.center.map("Center: " + _).getOrElse("") }</div>
       <div>{ streamsBinding.bind }</div>
     </div>
-  }
 
-  @dom private def streamsBinding: Binding[Node] = {
+  @dom private def streamsBinding: Binding[Node] =
     <ul>
       {
       for (str <- Constants(vm.ss.bind.streams: _*))
         yield <li>{ streamBinding(str).bind } </li>
       }
     </ul>
-  }
 
-  @dom private def streamBinding(str: VmDataStream): Binding[Node] = {
+  @dom private def streamBinding(str: VmDataStream): Binding[Node] =
     <div>
-      <span style="font-weight: bold">{ str.strid }</span>
-      { str.name.map(" - " + _).getOrElse("") }
-      { str.description.map(" - " + _).getOrElse("") }
-      <div>{ str.chartStyle.map(" chartStyle=" + _).getOrElse("") }</div>
-      <div>{ str.mapStyle.map(" mapStyle=" + _).getOrElse("") }</div>
-
       <div>
-        <span style="font-weight: bold">Variables:</span>
+        <span style="font-weight: bold">{ str.strid }</span>
+        <!--
+        { str.name.map(" - " + _).getOrElse("") }
+        { str.description.map(" - " + _).getOrElse("") }
+        <div>{ str.chartStyle.map(" chartStyle=" + _).getOrElse("") }</div>
+        <div>{ str.mapStyle.map(" mapStyle=" + _).getOrElse("") }</div>
+        -->
         <ul>
           {
           for (v <- Constants(str.variables.getOrElse(List.empty): _*))
             yield <li>
               { v.name +
-                v.units.map( " units=" + _).getOrElse("") +
-                v.chartStyle.map( " chartStyle=" + _).getOrElse("")
+                v.units.map(" (" + _ + ")").getOrElse("")
+                // + v.chartStyle.map( " chartStyle=" + _).getOrElse("")
               }
             </li>
           }
         </ul>
       </div>
       <div>
-        <span style="font-weight: bold">Observations:</span>
-        <ul>
-          {
+        {
           val obss = str.observations.getOrElse(Map.empty)
-          for (timestamp <- Constants(obss.keys.toSeq.sorted: _*))
-            yield <li>{ timestamp + " -> " + obss(timestamp).map(pp(_)) }</li>
-          }
-        </ul>
+          val timestamps = Constants(obss.keys.toSeq.sorted: _*)
+          <div>Observations: { timestamps.bind.length.toString }</div>
+          <div>Latest: { timestamps.bind.last }</div>
+          <!--ul>
+            {
+            for (timestamp <- timestamps)
+              yield <li>{ timestamp + " -> " + obss(timestamp).map(pp(_)) }</li>
+            }
+          </ul-->
+        }
       </div>
     </div>
-  }
 }
