2d12bb1c46da1660acfd6fdb69c5894e9e923479
Carlos Rueda <carueda@mbari.org>
2017-11-07 22:16:47 -0800

    some adjustments

--- docker/setenv.template.sh
+++ docker/setenv.template.sh
@@ -1,6 +1,7 @@
 export POSTGRES_DATA="$(pwd)/docker/postgres_data"
 export GPDVIZ_DB_USERNAME="gpdviz"
 export GPDVIZ_DB_USERPASS=???
+export GPDVIZ_DB_SERVERNAME="gpdviz_db"
 export GPDVIZ_HOST_POSTGRES_PORT=5432
 
 export GPDVIZ_HOST_PORT=5050
